1c79a5a195811a2f129026aa85d53304bdf078d9
Takumi Ishiyama <mt.stone20@gmail.com>
2022-12-28 18:01:45 +0900

    add symlink script

---
+++ symlink-dotfiles.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -u
+
+DIR="$(cd "$(dirname $0)" && pwd)"
+cd "$DIR"
+
+link-dotfiles() {
+  local files=(
+    .gitconfig
+    .commit_template
+    .tmux.conf
+    .zshrc
+    .config/starship.toml
+    .config/git/ignore
+    .config/nvim/init.vim
+    .config/nvim/dein.toml
+    .config/nvim/dein_lazy.toml
+    .config/karabiner/karabiner.json
+  )
+
+  for file in ${files[@]}; do
+    local src="$DIR/$file"
+    local dst="$HOME/$file"
+
+    echo "Creating symlink: $src => $dst"
+
+    local dir="$(dirname $file)"
+    local dst_dir="$HOME/$dir"
+    [[ ! -d "$dst_dir" ]] && mkdir -p "$dst_dir"
+
+    ln -sf "$src" "$dst"
+  done
+}
+
+run() {
+  link-dotfiles
+}
+
+run
+
