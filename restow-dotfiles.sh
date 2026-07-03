e4b473fabb6ac681b4817f5a4d42441cb333782b
Takumi Ishiyama <mt.stone20@gmail.com>
2026-07-04 03:22:23 +0900

    Add Herdr stow config

--- restow-dotfiles.sh
+++ restow-dotfiles.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 cd "$(dirname "$0")"
-stow -R -d stow -t "$HOME" brew zsh tmux git nvim gh ghostty starship karabiner hammerspoon misc
+stow -R -d stow -t "$HOME" brew zsh tmux git nvim gh ghostty starship karabiner hammerspoon misc herdr
